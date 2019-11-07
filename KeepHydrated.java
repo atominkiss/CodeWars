e358dbf864c1285f69a147805a7d2f6ad65c3231
atomink <atomink@gmail.com>
2019-11-07 23:09:34 +0300

    exercises from codewars 3 - Keep Hydrated

---
+++ KeepHydrated.java
@@ -0,0 +1,9 @@
+package net.protoprint;
+
+public class KeepHydrated {
+    public static int Liters(double time) {
+        //int water = (int) Math.floor(time * 0.5);
+
+        return (int) Math.floor(time * 0.5);
+    }
+}
--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println(DuplicateEncoder.encode("Annotation"));
 
-        System.out.println(JumpingNumber.jumpingNumber(32));
+        //System.out.println(JumpingNumber.jumpingNumber(32));
+
+        System.out.println(KeepHydrated.Liters(11.8));
     }
 }
