09649a47dd6f4f67192b1ce9ca129ab4d2bb5ae0
atomink <atomink@gmail.com>
2019-11-15 16:42:41 +0300

    exercises from codewars RegularExpressions

--- GiveMeADiamond.java
+++ GiveMeADiamond.java
@@ -7,34 +7,26 @@ public class GiveMeADiamond {
         int count = 1;
         String temp = "";
         String asterisk = "*";
+        String diamond = "";
 
-        if (n % 2 != 0 || n < 0) { //проверяем n на нечетность и отрицательность. Если n - положительное и нечетное то идем дальше
+        if (n % 2 != 0 && n > 0) { //проверяем n на нечетность и отрицательность. Если n - положительное и нечетное то идем дальше
             int k = (int) Math.ceil(((double) n) / 2);                 //Находим половину от числа, округляем в большую сторону.
-
-            //for (int i = 0; i < k; i++) {
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int j = 0; j < n; j++) {
-
-
-                str[j] = stringBuilder.append(asterisk).toString();
-                count += 2;
-                System.out.println(str[j]);
+            for (int i = 0; i < n; i++) {                               //Количество выводимых строк всегда равно введеному значению n
+                StringBuilder stringBuilder = new StringBuilder();
+                //количество символов в строке должно определяться этим значение и всегда долнжо быть нечетным
+                for (int j = 0; j < count; j++) {
+                    str[i] = stringBuilder.append(asterisk).toString();
                 }
-
-            //}
-
-
-//            for (int i = n; i >= 1; i = i - 2) {                    // идем с шагом 2
-//                StringBuilder stringBuilder = new StringBuilder();
-//                for (int j = 0; j < i; j++) {                       //
-//                    stringBuilder = stringBuilder.append(asterisk);
-//                }
-//
-//                System.out.println(stringBuilder);
-//
-//            }
-
-            return "нечетное число";
+                for (int m = 0; m < (n - str[i].length()) / 2; m++) { //заполняем пробелами с начала строки
+                    temp = temp + " ";
+                }
+                str[i] = temp + str[i] + "\n"; //кладем итоговую строку
+                if (count < n && i < k) count += 2;
+                else count -= 2;
+                temp = "";
+                diamond = diamond + str[i];
+            }
+            return diamond;
         } else
             return null;
 
